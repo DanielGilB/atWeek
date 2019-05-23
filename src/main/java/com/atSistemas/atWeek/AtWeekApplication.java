930efb102407d3457ab7b4683f815de02bf58c62
Daniel Gil <danielgilbustillo@gmail.com>
2019-05-23 19:48:52 +0200

    Starter

---
+++ src/main/java/com/atSistemas/atWeek/AtWeekApplication.java
@@ -0,0 +1,13 @@
+package com.atSistemas.atWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atSistemas/atWeek/AtWeekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atSistemas.atWeek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtWeekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
