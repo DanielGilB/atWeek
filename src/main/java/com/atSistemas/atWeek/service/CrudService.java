6496370689a7d49e50589c24add089f017f3f153
Daniel Gil <danielgilbustillo@gmail.com>
2019-05-25 16:38:10 +0200

    bug required bean on ServiceImp

--- src/main/java/com/atSistemas/atWeek/service/CrudService.java
+++ src/main/java/com/atSistemas/atWeek/service/CrudService.java
@@ -1,5 +1,8 @@
 package com.atSistemas.atWeek.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CrudService<T, ID> {
 
     public T create(T t);
--- src/main/java/com/atSistemas/atWeek/service/car/CarService.java
+++ src/main/java/com/atSistemas/atWeek/service/car/CarService.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface CarService extends CrudService<Car, Integer> {
+
 }
