94a16d7ee330af03c8db1262a93800f01f57e51a
Daniel Gil <danielgilbustillo@gmail.com>
2019-06-01 13:12:35 +0200

    rental dto accessing client dto and car dto

--- src/main/java/com/atSistemas/atWeek/mapper/rental/RentalMapperImp.java
+++ src/main/java/com/atSistemas/atWeek/mapper/rental/RentalMapperImp.java
@@ -1,7 +1,10 @@
 package com.atSistemas.atWeek.mapper.rental;
 
+import com.atSistemas.atWeek.mapper.car.CarMapper;
+import com.atSistemas.atWeek.mapper.client.ClientMapper;
 import com.atSistemas.atWeek.model.dto.RentalDTO;
 import com.atSistemas.atWeek.model.entity.Rental;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -13,6 +16,13 @@ import java.util.stream.Collectors;
 
 @Component
 public class RentalMapperImp implements RentalMapper {
+
+    @Autowired
+    private ClientMapper clientMapper;
+
+    @Autowired
+    private CarMapper carMapper;
+
     @Override
     public Rental map(RentalDTO dto) {
 
@@ -21,8 +31,6 @@ public class RentalMapperImp implements RentalMapper {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-[uuuu][uu]");
 
         rentalDTO.map(RentalDTO::getId).ifPresent(rental::setId);
-        rentalDTO.map(RentalDTO::getCar).ifPresent(rental::setCar);
-        rentalDTO.map(RentalDTO::getClient).ifPresent(rental::setClient);
         rentalDTO.map(RentalDTO::getPrice).ifPresent(rental::setPrice);
         rentalDTO.map(RentalDTO::getStartDate).ifPresent(date -> {
                     try { rental.setStartDate(LocalDate.parse(dto.getStartDate(), formatter)); }
@@ -33,8 +41,17 @@ public class RentalMapperImp implements RentalMapper {
                     catch (DateTimeParseException e){ throw e; }
                 });
 
+        rentalDTO.map(RentalDTO::getClient).ifPresent( client -> {
+            rental.setClient(clientMapper.map(client));
+        });
+
+        rentalDTO.map(RentalDTO::getCar).ifPresent( car -> {
+            rental.setCar(carMapper.map(car));
+        });
+
         return rental;
     }
+
 
     @Override
     public RentalDTO map(Rental rental) {
@@ -44,14 +61,21 @@ public class RentalMapperImp implements RentalMapper {
 
         rentalEntity.map(Rental::getId).ifPresent(dto::setId);
         rentalEntity.map(Rental::getPrice).ifPresent(dto::setPrice);
-        rentalEntity.map(Rental::getCar).ifPresent(dto::setCar);
-        rentalEntity.map(Rental::getClient).ifPresent(dto::setClient);
         rentalEntity.map(Rental::getStartDate).ifPresent(date -> {
                     dto.setStartDate(date.toString());
                 });
         rentalEntity.map(Rental::getEndDate).ifPresent(date -> {
                     dto.setEndDate(date.toString());
                 });
+
+
+        rentalEntity.map(Rental::getClient).ifPresent( client -> {
+                dto.setClient(clientMapper.map(client));
+            });
+
+        rentalEntity.map(Rental::getCar).ifPresent( car -> {
+            dto.setCar(carMapper.map(car));
+        });
 
         return dto;
     }
--- src/main/java/com/atSistemas/atWeek/model/dto/RentalDTO.java
+++ src/main/java/com/atSistemas/atWeek/model/dto/RentalDTO.java
@@ -1,17 +1,14 @@
 package com.atSistemas.atWeek.model.dto;
 
-import com.atSistemas.atWeek.model.entity.Car;
-import com.atSistemas.atWeek.model.entity.Client;
 import lombok.Data;
 
 @Data
 public class RentalDTO {
 
-
     private Integer id;
     private String startDate;
     private String endDate;
     private Double price;
-    private Car car;
-    private Client client;
+    private CarDTO car;
+    private ClientDTO client;
 }
